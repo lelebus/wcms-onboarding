c050ab7a9e02d55ebcfecc8f91fff0713efd5ead
Gabriele De Candido <gabrieledecandido@flashbeing.com>
2023-11-22 14:20:17 +0100

    finish import laite

--- matching_selection.py
+++ matching_selection.py
@@ -1,15 +1,9 @@
 import pandas as pd
 
 
-def clean_matches(dataframe):
-    print(f'Total rows: {dataframe.shape[0]}')
-
-    dataframe.head()
-
-    # take only correct matches
-    wines = dataframe[dataframe['ok'] == 1]
-    print(f'Total selected wines: {wines.shape[0]}')
-    print()
+def clean_rows(dataframe):
+    wines = dataframe.copy()
+    print(f'Total rows: {wines.shape[0]}')
 
     # price to cents or to zero, if no price
     wines['price'] = wines['price'].fillna(0)
@@ -23,7 +17,7 @@ def clean_matches(dataframe):
     wines['vintage'] = wines['vintage'].apply(lambda x: int(x))
 
     # clean info
-    wines = wines[['matched_id', 'external_id', 'size', 'vintage',
+    wines = wines[['ok', 'matched_id', 'external_id', 'size', 'vintage',
                    'price', 'info', 'storage_area', 'quantity', 'internal_notes']]
     wines = wines.rename(columns={'matched_id': 'wine_id'})
 
@@ -48,20 +42,46 @@ def generate_insert_file(filename):
         - 'internal_notes'
     """
     matches = pd.read_csv(filename)
-    wines = clean_matches(matches)
+    wines = clean_rows(matches)
+
+    # matched wines
+    selected_wines = wines[wines['ok'] == 1]
+    selected_wines = selected_wines.drop(columns=['ok'])
+    print(f'Total selected wines: {selected_wines.shape[0]}')
+    print()
 
     # remove duplicates
-    winesWithValidVintage = wines[wines['vintage'] != 0]
+    winesWithValidVintage = selected_wines[selected_wines['vintage'] != 0]
     duplicates = winesWithValidVintage[winesWithValidVintage.duplicated(
         subset=['wine_id', 'size', 'vintage'], keep=False)].sort_values(by=['wine_id', 'size', 'vintage'])
-    
-    # net wines are wines without duplicates    
-    net_wines = wines[~wines.index.isin(duplicates.index)]
 
+    # net wines are wines without duplicates
+    net_wines = selected_wines[~selected_wines.index.isin(duplicates.index)]
     print(f'Total net rows: {net_wines.shape[0]}')
 
     # Write CSV files
     net_wines.to_csv('v4-insert.csv', index=False)
 
     if duplicates.shape[0] > 0:
-        duplicates.to_csv('v4-duplicates.csv', index=False)
+        duplicates.to_csv('v4-insert-duplicates.csv', index=False)
+
+    # mismatched wines
+    mismatched_wines = wines[wines['ok'] == 0]
+    mismatched_wines = mismatched_wines.drop(columns=['ok'])
+    print(f'Total mismatched wines: {mismatched_wines.shape[0]}')
+    print()
+
+    # remove duplicates
+    winesWithValidVintage = mismatched_wines[mismatched_wines['vintage'] != 0]
+    duplicates = winesWithValidVintage[winesWithValidVintage.duplicated(
+        subset=['wine_id', 'size', 'vintage'], keep=False)].sort_values(by=['wine_id', 'size', 'vintage'])
+
+    # net wines are wines without duplicates
+    net_wines = mismatched_wines[~mismatched_wines.index.isin(
+        duplicates.index)]
+
+    # Write CSV files
+    net_wines.to_csv('v4-mismatched.csv', index=False)
+
+    if duplicates.shape[0] > 0:
+        duplicates.to_csv('v4-mismatched-duplicates.csv', index=False)

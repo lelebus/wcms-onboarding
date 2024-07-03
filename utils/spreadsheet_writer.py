980f143cb64bafae6f9517bca3a159d44be54416
michele98 <calvanese.michele@gmail.com>
2024-07-03 11:27:05 +0200

    feat: update creation of v3-selection-draft.ods
    now v3-selection-draft.ods only has 2 sheets: "selection" and "not-matched (DO NOT TOUCH)". In "selection" the user checks the wines that are correct, and the other one is just passed on to the next step as it is

--- utils/spreadsheet_writer.py
+++ utils/spreadsheet_writer.py
@@ -5,7 +5,7 @@ import odswriter as ods
 from utils import VColumns, fill_empty
 
 
-class V3SpreadsheetWriter:
+class V3SpreadsheetWriterOld:
     OUTPUT_FILENAME = "v3-selection-draft.ods"
 
     def __init__(self, root):
@@ -57,6 +57,47 @@ class V3SpreadsheetWriter:
             ):
                 if df is None:
                     continue
+                sheet = odsfile.new_sheet(sheet_name)
+                sheet.writerow(df.columns)
+                for _, row in df.fillna("").iterrows():
+                    sheet.writerow(row)
+
+
+class V3SpreadsheetWriter:
+    OUTPUT_FILENAME = "v3-selection-draft.ods"
+
+    def __init__(self, root, df_valid_matches, df_not_matched):
+        self.root = root
+        self.df_valid_matches, self.df_not_matched = self._fill_sheets(
+            df_valid_matches, df_not_matched
+        )
+        self._save()
+
+    @classmethod
+    def _fill_ok_formulas(cls, df):
+        return [ods.Formula(f"NOT(ISBLANK(B{i}))") for i in range(2, len(df) + 2)]
+
+    @classmethod
+    def _fill_ok(cls, df):
+        """To be modified for finding sure matches"""
+        return [False for _ in range(len(df))]
+
+    def _fill_sheets(self, df_valid_matches, df_not_matched):
+        df_valid_matches = fill_empty(df_valid_matches, VColumns.v3_selection())
+        df_valid_matches["ok"] = self._fill_ok(df_valid_matches)
+
+        df_not_matched = fill_empty(df_not_matched, VColumns.v3_not_found())
+        df_not_matched["ok"] = self._fill_ok_formulas(df_not_matched)
+
+        return df_valid_matches, df_not_matched
+
+    def _save(self):
+        print(f"Saving to {os.path.join(self.root, self.OUTPUT_FILENAME)}")
+        with ods.writer(os.path.join(self.root, self.OUTPUT_FILENAME)) as odsfile:
+            for sheet_name, df in zip(
+                ["selection", "not-matched (DO NOT TOUCH)"],
+                [self.df_valid_matches, self.df_not_matched],
+            ):
                 sheet = odsfile.new_sheet(sheet_name)
                 sheet.writerow(df.columns)
                 for _, row in df.fillna("").iterrows():
